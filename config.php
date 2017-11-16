bfdb63c893e02639462113de36a7bfd3adbecc23
litsazib <roktimsazib92@gmail.com>
2017-11-16 10:41:55 +0600

    init theme

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * The base MySQL settings of Osclass
+ */
+define('MULTISITE', 0);
+
+/** MySQL database name for Osclass */
+define('DB_NAME', 'osclassdb');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'root');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Table prefix */
+define('DB_TABLE_PREFIX', 'oc_site1');
+
+define('REL_WEB_URL', '/osclass/');
+
+define('WEB_PATH', 'http://localhost/osclass/');
+
+?>
